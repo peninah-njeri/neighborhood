a43084d057ed47f89736f13bea016fb1720cbd5f
peninah-njeri <peninahgathuru@gmail.com>
2019-12-01 12:33:17 +0300

    Create new project neighborhood and an app namely hood

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pesh/Documents/njeri-moringa-school-projects/Hood_Info/venv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
