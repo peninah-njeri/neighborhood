315fb7ca18b2f9ef7b45f8904e52732a940de25c
peninah-njeri <peninahgathuru@gmail.com>
2019-12-01 14:42:44 +0300

    register my app models to the admin

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import NeighborHood,Userprofile,Business
 
 # Register your models here.
+
+admin.site.register(NeighborHood)
+admin.site.register(Userprofile)
+admin.site.register(Business)
+
