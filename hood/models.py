7df1920cce2e2f7146e5d3bc22c14c84988a3b77
peninah-njeri <peninahgathuru@gmail.com>
2019-12-01 13:52:12 +0300

    create three models neighbourhood,business and user

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class NeighbourHood(models.Model):
+    name=models.CharField(max_length =40)
+    location=models.CharField(max_length=40)
+    occupants_count=models.PositiveIntegerField(blank=True,null=True)
+
+
+
+class Userprofile(models.Model):
+    user_name=models.OneToOneField(User,null = True,on_delete=models.CASCADE,related_name = "user")
+    neighborhood=models.ForeignKey(NeighborHood,on_delete=models.CASCADE,blank=True,null=True)
+    email=models.EmailField(blank=True,null=True)
+
+
+
+class Business(models.Model):
+    business_name=models.CharField(max_length =30)
+    user=models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
+    neighborhood=models.ForeignKey(NeighborHood,on_delete=models.CASCADE,blank=True,null=True)
+    email=models.EmailField(blank=True,null=True)    
+
