ee6f5a4b39938879d5ca27d187fb1df713989b9a
peninah-njeri <peninahgathuru@gmail.com>
2019-12-02 22:53:05 +0300

    add more views to match user requests

--- hood/views.py
+++ hood/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Business,NeighborHood,Userprofile,Post,PoliceCenters,HealthCenter,Comment
+from .forms import NewProfileForm,NewNeighborhoodForm,UpdateForm,NewPostForm,NewBusinessForm,NewCommentForm
 
 # Create your views here.
 def homepage(request):
@@ -9,8 +11,53 @@ def homepage(request):
     neighborhoods=NeighborHood.get_all_neighborhoods()
     posts=Post.get_all_posts()
 
-   
-    return render(request,'homepage.html',{"businesses":businesses,"neighborhoods":neighborhoods,"posts":posts})
+    current_user=request.user
+    if request.method == 'POST':
+        form =NewCommentForm(request.POST,request.FILES)
+        if form.is_valid():
+            comment=form.save(commit=False)
+
+            comment.commenter = current_user
+
+            comment.save()
+        return redirect('homepage')
+    else:
+        form=NewCommentForm()
+
+
+    return render(request,'homepage.html',{"businesses":businesses,"neighborhoods":neighborhoods,"posts":posts,"form":form})
+
+
+def comments(request):
+
+    comments=Comment.get_comments()
+
+    return render(request,'comments.html',{"comments":comments})
+
+def new_comment(request):
+    current_user=request.user
+    if request.method == 'POST':
+        form =NewCommentForm(request.POST,request.FILES)
+        if form.is_valid():
+            comment=form.save(commit=False)
+
+            comment.commenter = current_user
+
+            comment.save()
+        return redirect('homepage')
+    else:
+        form=NewCommentForm()
+    return render(request,'comment.html',{"form":form})
+
+
+def contacts(request):
+    police=PoliceCenters.get_all_police()
+    health=HealthCenter.get_all_health()
+#
+    return render(request,'contact.html',{"police":police,"health":health})
+
+def neighbourhood(request,neighborhood_id):
+    businesses=Business.objects.filter(neighborhood_id=neighborhood_id)
 
 
 
@@ -19,28 +66,6 @@ def neighborhood(request,neighborhood_id):
 
     return render(request,'neighborhood.html',{"neighborhood":neighborhood})
 
-
-
-
-
-def business(request):
-    current_user=request.user
-
-    if request.method == 'POST':
-        form =NewBusinessForm(request.POST,request.FILES)
-        if form.is_valid():
-            business=form.save(commit=False)
-            business.user = current_user
-            business.save()
-        return redirect('index')
-    else:
-        form=NewBusinessForm()
-    return render(request,'business.html',{"form":form})
-
-def neighbourhood(request,neighborhood_id):
-    businesses=Business.objects.filter(neighborhood_id=neighborhood_id)
-
-    return render(request,'neighbourhood.html',{"businesses":businesses})    
 
 
 def post(request):
@@ -58,3 +83,99 @@ def post(request):
     return render(request,'post.html',{"form":form})
 
 
+
+
+def business(request):
+    current_user=request.user
+
+    if request.method == 'POST':
+        form =NewBusinessForm(request.POST,request.FILES)
+        if form.is_valid():
+            business=form.save(commit=False)
+            business.user = current_user
+            business.save()
+        return redirect('homepage')
+    else:
+        form=NewBusinessForm()
+    return render(request,'business.html',{"form":form})
+
+
+
+def profile(request):
+    current_user=request.user
+    userprofile = Userprofile.objects.filter(user_name=current_user)
+
+    if len(userprofile)<1:
+        userprofile = "No profile"
+    else:
+        userprofile = Userprofile.objects.get(user_name=current_user)
+    return render(request,'profile.html',{"userprofile":userprofile})
+
+
+def edit_profile(request):
+    current_user=request.user
+
+    if request.method == 'POST':
+        form =NewProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            userprofile=form.save(commit=False)
+            userprofile.user_name = current_user
+            userprofile.save()
+        return redirect('profile')
+    else:
+        form=NewProfileForm()
+    return render(request,'edit_profile.html',{"form":form})
+
+
+
+
+
+def update_neighborhood(request):
+    current_user=request.user
+
+    if request.method == 'POST':
+        form =NewNeighborhoodForm(request.POST,request.FILES)
+        if form.is_valid():
+            userprofile=form.save(commit=False)
+            userprofile.user_name = current_user
+            userprofile.save()
+        return redirect('profile')
+    else:
+        form=NewNeighborhoodForm()
+    return render(request,'update_neighborhood.html',{"form":form})
+
+
+
+
+def update_profile(request,id):
+    instance=get_object_or_404(Userprofile,id=id)
+    form=UpdateForm(request.POST or None,instance=instance)
+    if form.is_valid():
+        form.save
+        return redirect('profile')
+
+    return render(request,'update_profile.html', {'form': form})
+
+
+
+
+
+def search_results(request):
+
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        searched_businesses = Business.search_by_business_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"businesses": searched_businesses})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+
+def businessdetails(request,business_id):
+
+    business=Business.objects.get(id=business_id)
+
+    return render(request,"businessdetails.html",{"business":business})
