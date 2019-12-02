48deee3d867f67a8467bcb33d8e179f0ccb26a4b
peninah-njeri <peninahgathuru@gmail.com>
2019-12-02 15:37:33 +0300

    create a view function to for a user to post a random post

--- hood/views.py
+++ hood/views.py
@@ -13,3 +13,35 @@ def homepage(request):
     return render(request,'homepage.html',{"businesses":businesses,"neighborhoods":neighborhoods,"posts":posts})
 
 
+
+
+def business(request):
+    current_user=request.user
+
+    if request.method == 'POST':
+        form =NewBusinessForm(request.POST,request.FILES)
+        if form.is_valid():
+            business=form.save(commit=False)
+            business.user = current_user
+            business.save()
+        return redirect('index')
+    else:
+        form=NewBusinessForm()
+    return render(request,'business.html',{"form":form})
+
+
+def post(request):
+    current_user=request.user
+
+    if request.method == 'POST':
+        form =NewPostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.owner = current_user
+            post.save()
+        return redirect('homepage')
+    else:
+        form=NewPostForm()
+    return render(request,'post.html',{"form":form})
+
+
