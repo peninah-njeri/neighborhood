18c5811982071163d36a3ea52227be6bad0923ae
peninah-njeri <peninahgathuru@gmail.com>
2019-12-03 16:55:37 +0300

    add more tests to test all my models

--- hood/models.py
+++ hood/models.py
@@ -1,18 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 
 class NeighborHood(models.Model):
+
     neighborhood_image=models.ImageField(upload_to='neighborhoods',null=True)
     name=models.CharField(max_length =40)
     location=models.CharField(max_length=40)
     occupants_count=models.PositiveIntegerField(blank=True,null=True)
 
-
     def __str__(self):
         return self.name
 
+    def save_neighborhood(self):
+        self.save()
+    def delete_neighborhood(self):
+        self.delete()
+
+    def update_neighborhood(self):
+        self.neighborhood_image=neighborhood_image
+        self.name=name
+        self.location=location
+        self.occupants_count=occupants_count
+        self.save()
+
+
+    def update_occupants(self,occupants_count):
+        self.occupants_count=occupants_count
+        self.save()
+
+
+    @classmethod
+    def find_neighborhood_by_id(cls,neighborhood_id):
+        neighborhood=cls.objects.get(id=neighborhood_id)
+        return neighborhood
 
     @classmethod
     def get_all_neighborhoods(cls):
@@ -22,6 +45,7 @@ class NeighborHood(models.Model):
 
 
 class Userprofile(models.Model):
+
     profile_image=models.ImageField(upload_to='userprofiles',null=True)
     user_name=models.OneToOneField(User,null = True,on_delete=models.CASCADE,related_name = "user")
     neighborhood=models.ForeignKey(NeighborHood,on_delete=models.CASCADE,blank=True,null=True)
@@ -29,19 +53,61 @@ class Userprofile(models.Model):
 
 
 
+    def save_userprofile(self):
+        self.save()
+    def delete_userprofile(self):
+        self.delete()
+
+    def update_neighborhood(self,neighborhood):
+        self.neighborhood=neighborhood
+        self.save()
+        # NeighborHood.objects.filter(id=userprofile_id).update(neighborhood="neighborhood_id")
+
 class Business(models.Model):
     business_image=models.ImageField(upload_to='businesses',null=True)
     business_name=models.CharField(max_length =30)
     user=models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
     neighborhood=models.ForeignKey(NeighborHood,on_delete=models.CASCADE,blank=True,null=True)
-    email=models.EmailField(blank=True,null=True)    
+    email=models.EmailField(blank=True,null=True)
 
+    def save_business(self):
+        self.save()
+    def delete_business(self):
+        self.delete()
+
+
+    def update_business(self):
+        self.business_image=business_image
+        self.business_name=business_name
+        self.user=location
+        self.neighborhood=neighborhood
+        self.email=email
+        self.save()
+
+    @classmethod
+    def filter_by_neighborhood(cls,neighborhood_id):
+        businesses=cls.objects.filter(id=neighborhood_id)
+        return businesses
+
+
+    @classmethod
+    def find_business_by_id(cls,business_id):
+        business=cls.objects.get(id=business_id)
+        return business
 
 
     @classmethod
     def get_all_businesses(cls):
         businesses=cls.objects.all()
         return businesses
+
+
+    @classmethod
+    def search_by_business_name(cls,search_term):
+        businesses = cls.objects.filter(business_name__icontains=search_term)
+        return businesses
+
+
 
 class Post(models.Model):
     post_image=models.ImageField(upload_to='posts',null=True)
@@ -50,7 +116,6 @@ class Post(models.Model):
     neighborhood=models.ForeignKey(NeighborHood,on_delete=models.CASCADE,blank=True,null=True)
     post=models.TextField()
 
-
     def __str__(self):
         return self.title
 
@@ -58,20 +123,30 @@ class Post(models.Model):
     def get_all_posts(cls):
         posts=cls.objects.all()
         return posts
+    def save_post(self):
+        self.save()
+    def delete_post(self):
+        self.delete()
 
-
-
-   
 class HealthCenter(models.Model):
     name = models.CharField(max_length = 50)
     location=models.CharField(max_length =50)
     contact=models.CharField(max_length=40)
 
+    @classmethod
+    def get_all_health(cls):
+        health=cls.objects.all()
+        return health
 
 class PoliceCenters(models.Model):
     name = models.CharField(max_length = 50)
     location=models.CharField(max_length =50)
     contact=models.CharField(max_length=40)
+
+    @classmethod
+    def get_all_police(cls):
+        police=cls.objects.all()
+        return police
 
 
 
@@ -82,12 +157,37 @@ class Comment(models.Model):
 
 
 
+    def delete_comment(self):
+        self.delete()
+
+    def save_comment(self):
+        self.save()
+
+    @classmethod
+    def get_comments(cls):
+        comments=cls.objects.all()
+        return comments
+
+    @classmethod
+    def get_comments_by_post_id(cls,post_id):
+        comments=cls.objects.filter(id=post_id)
+        return comments
+
+
 class Category(models.Model):
     name=models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name_plural = "Categories"
 
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
 
 class Location(models.Model):
     name=models.CharField(max_length=30)
@@ -95,3 +195,9 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,134 @@
 from django.test import TestCase
+from .models import Business,NeighborHood,Userprofile,Post,Comment
+
 
 # Create your tests here.
+class NeighborHoodTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.new_neighborhood =NeighborHood(neighborhood_image="image.jpeg",name="Moringa",location="Ngong road",occupants_count=20)
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_neighborhood,NeighborHood))
+
+    #Testing Save Method
+    def test_save_method(self):
+        self.new_neighborhood.save_neighborhood()
+        neighborhoods=NeighborHood.objects.all()
+        self.assertTrue(len(neighborhoods)>0)
+
+    def test_delete_method(self):
+        self.new_neighborhood.save_neighborhood()
+        self.new_neighborhood.delete_neighborhood()
+
+
+    def test_find_neighborhood_by_id(self):
+        neighborhood=NeighborHood.find_neighborhood_by_id()
+        self.assertTrue(len(neighborhood)>0)
+
+    def test_update_occupants(self):
+        self.new_neighborhood.save_neighborhood()
+        self.new_neighborhood = NeighborHood.objects.filter(id=1)
+        neighborhood = self.new_neighborhood
+        Neighborhood.update_occupants(40)
+        self.updated_neighborhood = NeighborHood.objects.get(id=1)
+        self.assertEqual(self.updated_neighborhood.occupants_count,40)
+
+
+
+
+class BuinessTestClass(TestCase):
+
+
+    def setUp(self):
+        self.new_business=Business(business_image="image.jpeg",business_name="Masoko",email="masoko@gmail.com")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_business,Business))
+
+
+    def test_save_method(self):
+
+        self.new_business.save_business()
+        businesses = Business.objects.all()
+        self.assertTrue(len(businesses) > 0)
+
+    def test_delete_method(self):
+
+        self.new_business.save_business()
+        self.new_business.delete_business()
+
+    def test_find_business_by_id(cls,business_id):
+        business=Business.find_business_by_id()
+        self.assertTrue(len(business)>0)
+
+
+
+class UserprofileTestClass(TestCase):
+    #Set up method
+    def setUp(self):
+        self.new_userprofile =Userprofile(profile_image="image.jpeg",email="profile@gmail.com")
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_userprofile,Userprofile))
+
+    #Testing Save Method
+    def test_save_method(self):
+        self.new_userprofile.save_userprofile()
+        userprofiles=Userprofile.objects.all()
+        self.assertTrue(len(userprofiles)>0)
+
+    def test_delete_method(self):
+        self.new_userprofile.save_userprofile()
+        self.new_userprofile.delete_userprofile()
+
+
+
+class PostTestClass(TestCase):
+
+
+    def setUp(self):
+        self.new_post=Post(post_image="image.jpeg",title="Security",post="Don't waste water")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_post,Post))
+
+
+    def test_save_method(self):
+
+        self.new_post.save_post()
+        posts = Post.objects.all()
+        self.assertTrue(len(posts) > 0)
+
+    def test_delete_method(self):
+
+        self.new_post.save_post()
+        self.new_post.delete_post()
+
+
+class CommentTestCase(TestCase):
+    '''
+    setup
+    '''
+    def setUp(self):
+        self.new_comment = Comment(comment='Thank you')
+    '''
+    test instance of comment
+    '''
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_comment,Comment))
+        '''
+        test for save instance of comment
+        '''
+    def test_save_comment(self):
+        self.comment.save_comment()
+        name = Comment.objects.all()
+        self.assertTrue(len(name)>0)
+
+    def test_delete_method(self):
+
+        self.new_comment.save_comment()
+        self.new_comment.delete_comment()
+
